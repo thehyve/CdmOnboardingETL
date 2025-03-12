667337b83b2aaba376588e264fa4f1710fcda2e2
Maxim Moinat <maximmoinat@gmail.com>
2025-03-12 10:02:48 +0100

    update description

--- R/generatePerformanceSection.R
+++ R/generatePerformanceSection.R
@@ -117,8 +117,10 @@ generatePerformanceSection <- function(doc, results) {
     indexOverview <- df$appliedIndexes$result %>%
       dplyr::mutate(actual = 1) %>%
       dplyr::mutate(type = substr(.data$INDEXNAME, 1, 3)) %>%
-      # Note: The join by does not accept the .data$ prefix
-      dplyr::full_join(expectedIndexes, by = join_by(TABLENAME, INDEXNAME, type)) %>%
+      # Note: The join_by by does not accept the .data$ prefix
+      dplyr::full_join(expectedIndexes,
+        by = dplyr::join_by("TABLENAME", "INDEXNAME", "type")
+      ) %>%
       dplyr::group_by(.data$TABLENAME, .data$type) %>%
       dplyr::summarize(
         n_indexes_applied = sum(.data$actual, na.rm = TRUE),
