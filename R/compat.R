5bb3c5c7324ce7c449565554037a7ab83d12c831
Maxim Moinat <maximmoinat@gmail.com>
2025-11-12 22:18:53 +0100

    add source concept name in top unmapped

--- R/compat.R
+++ R/compat.R
@@ -73,6 +73,7 @@ compat <- function(r, target_version = package_version('3.0')) {
   r$vocabularyResults$mappingCompleteness <- .fixDataFrameNames(r$vocabularyResults$mappingCompleteness)
   r$vocabularyResults$drugMapping <- .fixDataFrameNames(r$vocabularyResults$drugMapping)
 
+  # TODO: fix unmapped by adding source_concept_id and source_concept_name fields.
   r$vocabularyResults$unmappedDrugs <- .fixDataFrameNames(r$vocabularyResults$unmappedDrugs)
   r$vocabularyResults$unmappedConditions <- .fixDataFrameNames(r$vocabularyResults$unmappedConditions)
   r$vocabularyResults$unmappedMeasurements <- .fixDataFrameNames(r$vocabularyResults$unmappedMeasurements)
--- inst/sql/sql_server/checks/unmapped_concepts_templated.sql
+++ inst/sql/sql_server/checks/unmapped_concepts_templated.sql
@@ -3,10 +3,13 @@
 select top 25
 	ROW_NUMBER() OVER(ORDER BY num_records desc) as row_num,
 	source_value as source_value,
+  CAST(source_concept_id AS VARCHAR) as source_concept_id,
+  concept.concept_name as source_concept_name,
 	floor((num_records+99)/100)*100 as n_records,
 	100.0 * num_records/t.total_records as p_records
 from #@cdmDomain as cte
 cross join (select sum(num_records) as total_records from #@cdmDomain) t
+left join @cdmDatabaseSchema.concept as concept on cte.source_concept_id = concept.concept_id
 where is_mapped = 0 and num_records > @smallCellCount
 order by num_records desc
 ;
